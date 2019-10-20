04ef8cdc7dedb2c1333d1ed4be5c2260fe314b7e
saifilmaknojia <saifilmaknojia@gmail.com>
2019-10-20 00:43:42 -0400

    Q1 read input

---
+++ HW1_1.java
@@ -0,0 +1,13 @@
+class HW1_1 {
+    public static void main(String[] args) {
+
+        int sequenceLength = Integer.parseInt(args[0]);
+        int A = Integer.parseInt(args[1]);
+        int C = Integer.parseInt(args[2]);
+        int G = Integer.parseInt(args[3]);
+        int T = Integer.parseInt(args[4]);
+        int numberOfSequences = Integer.parseInt(args[5]);
+        float probability = Float.parseFloat(args[6]);
+        String outputFileName = args[7];
+    }
+}
