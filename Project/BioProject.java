14a37a0ba6796641862bf478b357f82732ddd909
Amruta <amruta.basrur@ufl.edu>
2019-11-27 17:37:07 -0500

    read input from file and execute Pomsa algorithm

---
+++ Project/BioProject.java
@@ -0,0 +1,100 @@
+import java.io.*;
+import java.util.*;
+
+class BioProject {
+	// static FileOutputStream output;
+
+	static List<StringBuilder> outputArray = new ArrayList<>();
+	static int threshold = 2;
+	public static void main(String[] args) throws IOException {
+
+		try{
+			List<StringBuilder> sequenceArray = new ArrayList<>();
+			int maxLength = 0;
+			int minLength = Integer.MAX_VALUE;
+			// creates a FileReader Object
+			File file = new File(args[0] + ".txt");
+			BufferedReader br = new BufferedReader(new FileReader(file));
+			String ip;
+			StringBuilder sb = null;
+			while ((ip = br.readLine()) != null) {
+				if (ip.charAt(0) == '>') {
+					if (sb != null) {
+						sequenceArray.add(sb);
+						maxLength = sb.length()>maxLength?sb.length():maxLength;
+						minLength = sb.length()<minLength?sb.length():minLength;
+					}
+					sb = new StringBuilder();
+				}
+				else {
+					sb.append(ip);
+				}
+			}
+
+			if (sb != null) {
+				maxLength = sb.length()>maxLength?sb.length():maxLength;
+				sequenceArray.add(sb);
+			}
+
+			br.close();
+
+			insertEndGaps(sequenceArray, maxLength);
+
+			Pomsa.executePomsa(sequenceArray, maxLength);
+
+			file = new File(args[1] + ".txt");
+
+			// creates the file
+			file.createNewFile();
+
+			// creates a FileWriter Object
+			FileWriter fwriter = new FileWriter(file, false);
+			BufferedWriter bwriter = new BufferedWriter(fwriter);
+			
+			for(int i=0;i<outputArray.size();i++) {
+				writeToFile(outputArray.get(i), bwriter);
+			}
+			bwriter.close();
+
+
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Unable to locate file " + args[0]);
+			e.printStackTrace();
+		}
+
+	}
+
+	static void insertEndGaps(List<StringBuilder> sequenceArray, int maxLength) {
+		// TODO Auto-generated method stub
+		for(StringBuilder st:sequenceArray) {
+			int n = st.length();
+			while(n<maxLength) {
+				st.append('.');
+				n++;
+			}
+		}
+	}
+	
+	 static void writeToFile(StringBuilder write, BufferedWriter br) {
+	        try {
+	            br.write('>');
+	            br.newLine();
+	            int i = 0;
+	            while (i < write.length()) {
+	                if (write.length() < i + 80) {
+	                    br.append(write.substring(i, write.length()));
+	                    br.newLine();
+	                    break;
+	                }
+	                br.append(write.substring(i, i + 80));
+	                i = i + 80;
+	                br.newLine();
+	            }
+	            br.flush();
+	        } catch (Exception e) {
+	            System.out.println("unable to write to file");
+	        }
+
+	    }
+}
---
+++ Project/Pomsa.java
@@ -0,0 +1,73 @@
+import java.util.*;
+
+class Pomsa {
+	// static FileOutputStream output;
+	
+	static void executePomsa(List<StringBuilder> sequenceArray, int maxLength) {
+		// TODO Auto-generated method stub
+		char dominant;
+		int newMaxLength = 0;
+		for(int i=0;i<maxLength;i++) {
+			for(int j=0;j<sequenceArray.size();j++) {
+				StringBuilder sb = sequenceArray.get(j);
+				if(i==maxLength-1 && sb.length()>newMaxLength ) {
+					newMaxLength = sb.length();
+				}
+				dominant = getDominant(sequenceArray, i);
+				if(dominant != '.' && sb.charAt(i) != dominant) {
+					if(i>0 && sb.charAt(i-1) == dominant) {
+						sb.insert(i-1, '.');
+					}
+					if(i<maxLength && sb.charAt(i+1) == dominant) {
+						for(int k=0;k<sequenceArray.size();k++) {
+							if(k!=j) {
+								sequenceArray.get(k).insert(i, '.');
+							}
+						}
+					}
+				}
+			}
+		}
+		BioProject.insertEndGaps(sequenceArray, newMaxLength);
+		for(int i=0;i<sequenceArray.size();i++)
+		{
+			if(BioProject.outputArray.size()<sequenceArray.size()) {
+				BioProject.outputArray.add(sequenceArray.get(i));
+			}
+			else {
+				BioProject.outputArray.get(i).append(sequenceArray.get(i));
+			}
+		}
+
+	}
+
+	private static char getDominant(List<StringBuilder> sequenceArray, int col) {
+		// TODO Auto-generated method stub
+		Map<Character, Integer> count = new HashMap<Character, Integer>() {/**
+			 * 
+			 */
+			private static final long serialVersionUID = 1L;
+
+		{
+			put('A', 0);
+			put('C', 0);
+			put('G', 0);
+			put('T', 0);
+		}};
+		char curr;
+		for(int i=0;i<sequenceArray.size();i++) {
+			curr = sequenceArray.get(i).charAt(col);
+			count.put(curr, count.getOrDefault(curr, 0)+1);
+		}
+		int max = BioProject.threshold;
+		char retChar = '.';
+
+		for(char ch: count.keySet()) {
+			if(max < count.get(ch)) {
+				retChar = ch;
+				max = count.get(ch);
+			}
+		}
+		return retChar;
+	}
+}
