c754e2d471a5ccde79f86a0ba874162ea05d5dcf
saifilmaknojia <saifilmaknojia@gmail.com>
2019-12-02 23:26:15 -0500

    Updated sequence generator

--- Project/src/SequenceGenerator.java
+++ Project/src/SequenceGenerator.java
@@ -1,5 +1,3 @@
-// package Project;
-
 import java.io.*;
 import java.util.*;
 
@@ -28,24 +26,89 @@ class SequenceGenerator {
         int C = 50;
         int G = 75;
         // int T = 100;
-        for (int j = 0; j < numberOfSequences; j++) {
+
+        int low = 100;
+        int high = 175;
+        int sequenceLength = rand.nextInt(high - low) + low;
+        StringBuilder sequence = new StringBuilder();
+        // Generate first sequence
+        for (int i = 1; i <= sequenceLength; i++) {
+            int selected = rand.nextInt(100);
+            if (selected < A)
+                sequence.append("A");
+            else if (selected >= A && selected < C)
+                sequence.append("C");
+            else if (selected >= C && selected < G)
+                sequence.append("G");
+            else
+                sequence.append("T");
+        }
+        // System.out.println(sequence.length());
+        writeToFile(sequence, br);
+
+        float probability = 0.05f;
+        // generate remaining sequences;
+        for (int i = 0; i < numberOfSequences - 1; i++) {
+            int count_mutate = 0;
             // generate sequences
-            int low = 500;
-            int high = 600;
-            int sequenceLength = rand.nextInt(high - low) + low;
-            StringBuilder sequence = new StringBuilder();
-            for (int i = 1; i <= sequenceLength; i++) {
-                int selected = rand.nextInt(100);
-                if (selected < A)
-                    sequence.append("A");
-                else if (selected >= A && selected < C)
-                    sequence.append("C");
-                else if (selected >= C && selected < G)
-                    sequence.append("G");
-                else
-                    sequence.append("T");
+            sequenceLength = rand.nextInt(high - low) + low;
+
+            // If the new sequence length is greater than the previous sequence, mutate the
+            // first half and then update the remaining by following normal logic
+            if (sequenceLength > sequence.length()) {
+                // System.out.println("new length greater "+sequenceLength);
+                int remaining = sequenceLength - sequence.length();
+                for (int j = 0; j < sequence.length(); j++) {
+                    // StringBuilder sequence = new StringBuilder();
+                    float mutate = rand.nextFloat();
+                    if (mutate < probability) {
+                        count_mutate++;
+                        int selected = rand.nextInt(100);
+                        if (selected < A)
+                            sequence.setCharAt(j, 'A');
+                        else if (selected >= A && selected < C)
+                            sequence.setCharAt(j, 'C');
+                        else if (selected >= C && selected < G)
+                            sequence.setCharAt(j, 'G');
+                        else
+                            sequence.setCharAt(j, 'T');
+                    }
+                }
+
+                for (int k = 1; k <= remaining; k++) {
+                    int selected = rand.nextInt(100);
+                    if (selected < A)
+                        sequence.append("A");
+                    else if (selected >= A && selected < C)
+                        sequence.append("C");
+                    else if (selected >= C && selected < G)
+                        sequence.append("G");
+                    else
+                        sequence.append("T");
+                }
             }
-            System.out.println(sequence.length());
+            // else part where new sequence length <= previous sequence length
+            else {
+                for (int j = 0; j < sequenceLength; j++) {
+                    // StringBuilder sequence = new StringBuilder();
+                    float mutate = rand.nextFloat();
+                    if (mutate < probability) {
+                        count_mutate++;
+                        int selected = rand.nextInt(100);
+                        if (selected < A)
+                            sequence.setCharAt(j, 'A');
+                        else if (selected >= A && selected < C)
+                            sequence.setCharAt(j, 'C');
+                        else if (selected >= C && selected < G)
+                            sequence.setCharAt(j, 'G');
+                        else
+                            sequence.setCharAt(j, 'T');
+                    }
+                }
+            }
+
+            // System.out.println("Sequence Length "+sequenceLength);
+            System.out.println("Mutated " + count_mutate);
             writeToFile(sequence, br);
         }
     }
